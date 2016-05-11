3843b4608e147ff7b9f4cd48e2f4ee112fb298bd
e-e-e <e-e-e@users.noreply.github.com>
2016-05-11 12:00:23 +1000

    Added how to install info + notification of new messages

--- html/src/client/odp-0.0.1.js
+++ html/src/client/odp-0.0.1.js
@@ -145,7 +145,7 @@
 
 	// STUPID HACK FOR NEXTWAVE URLS
 
-	var nextwave_urls = {"#event=141":"/events/panel-can-ecosex-save-earth/","#event=155":"/events/a97/","#event=159":"/events/admission-into-the-everyday-sublime/","#event=211":"/events/camel/","#event=212":"/events/mummy-dearest/","#event=213":"/events/sedihsunno/","#event=214":"/events/under-my-skin/","#event=270":"/events/algorthmic-misfits/","#event=271":"/events/angkot-alien/","#event=273":"/events/ua-numi-le-fau/","#event=274":"/events/blaaq-catt/","#event=275":"/events/love-five-feminist-perspectives/","#event=276":"/events/desert-body-creep/","#event=277":"/events/telltale/","#event=278":"/events/still-i-rise/","#event=279":"/events/something-less/","#event=280":"/events/fempress/","#event=281":"/events/sisters-akousmatica/","#event=282":"/events/ground-control/","#event=283":"/events/ships-in-the-night/","#event=284":"/events/the-horse/","#event=285":"/events/shadow-sites/","#event=287":"/events/shadows-on-the-hill/","#event=288":"/events/microlandscapes/","#event=289":"/events/separating-hydrogen-air-primer/","#event=290":"/events/misconceive/","#event=295":"/events/the-second-woman/","#event=296":"/events/one-million-views/","#event=297":"/events/relating-immediate-surroundings-something-im-talking/","#event=298":"/events/passing/","#event=300":"/events/our-disappearing-present/","#event=301":"/events/the-fraud-complex/","#event=302":"/events/voices-joan-arc/","#event=303":"/events/far-from-here/","#event=305":"/events/decolonist/","#event=304":"/events/blaksland-and-lawless/","#event=306":"/events/arrival-of-the-rajah/","#event=453":"/events/truth-symposium-internet-privacy/","#event=563":"/events/shadow-sites-artist-talk-and-walking-tour/","#event=594":"/events/one-million-views-walking-tour/","#event=596":"/events/one-million-views-workshop/","#event=624":"/events/decolonist-artist-talk-meditation-and-workshop/","#event=700":"/events/indigenous-language-workshops/","#event=701":"/events/festival-club/"};
+	var nextwave_urls = {"#event=141":"/events/panel-can-ecosex-save-earth/","#event=155":"/events/a97/","#event=159":"/events/admission-into-the-everyday-sublime/","#event=211":"/events/camel/","#event=212":"/events/mummy-dearest/","#event=213":"/events/sedihsunno/","#event=214":"/events/under-my-skin/","#event=270":"/events/algorthmic-misfits/","#event=271":"/events/angkot-alien/","#event=273":"/events/ua-numi-le-fau/","#event=274":"/events/blaaq-catt/","#event=275":"/events/love-five-feminist-perspectives/","#event=276":"/events/desert-body-creep/","#event=277":"/events/telltale/","#event=278":"/events/still-i-rise/","#event=279":"/events/something-less/","#event=280":"/events/fempress/","#event=281":"/events/sisters-akousmatica/","#event=282":"/events/ground-control/","#event=283":"/events/ships-in-the-night/","#event=284":"/events/the-horse/","#event=285":"/events/shadow-sites/","#event=287":"/events/shadows-on-the-hill/","#event=288":"/events/microlandscapes/","#event=289":"/events/separating-hydrogen-air-primer/","#event=290":"/events/misconceive/","#event=295":"/events/the-second-woman/","#event=296":"/events/one-million-views/","#event=297":"/events/relating-immediate-surroundings-something-im-talking/","#event=298":"/events/passing/","#event=300":"/events/our-disappearing-present/","#event=301":"/events/the-fraud-complex/","#event=302":"/events/voices-joan-arc/","#event=303":"/events/far-from-here/","#event=305":"/events/decolonist/","#event=304":"/events/blaksland-and-lawless/","#event=306":"/events/arrival-of-the-rajah/","#event=453":"/events/truth-symposium-internet-privacy/","#event=563":"/events/shadow-sites-artist-talk-and-walking-tour/","#event=594":"/events/one-million-views-walking-tour/","#event=596":"/events/one-million-views-workshop/","#event=624":"/events/decolonist-artist-talk-meditation-and-workshop/","#event=700":"/events/indigenous-language-workshops/","#event=701":"/events/festival-club/", "#event=133":"/events/ecosexual-bathhouse/"};
 
 	// ----- MAIN -----
 
@@ -395,8 +395,8 @@
 		el.hide().appendTo('body');
 		
 		// attach event handers
-		
-		el.find('#notify-bottom').click(this._scroll_to_bottom.bind(this));
+		el.find('#odp-notify-bottom').click(this._hide_msg_nofication.bind(this));
+		el.find('#odp-notify-bottom').click(this._scroll_to_bottom.bind(this));
 		// submit on keypress within textarea input
 		el.find(this.ids.input).on('keypress', this._keypress.bind(this));
 		// submit form
@@ -466,7 +466,7 @@
 		$(this.ids.window).slideUp();
 		$(this.ids.content).show();
 		$(this.ids.info).hide();
-		
+		this._hide_msg_nofication();
 		this.opened = false;
 		//do anything like don't listen for anymore odp messages
 	};
@@ -475,6 +475,7 @@
 		// visually shift tabs
 		var tab = $( event.target );
 		if(!tab.hasClass('active')) {
+			this._hide_msg_nofication();
 			$(this.ids.tabs).removeClass('active');
 			var rel = tab.data('odp-tab');
 			tab.addClass('active');
@@ -577,10 +578,27 @@
 		
 		if(bottom) this._scroll_to_bottom();
 		else {
-			//show alert.
+			// show alert.
 			console.log('NEW MESSAGE');
-			//$('#notify-bottom').text("OK").css({background:"red",color:"white"});
+			this._show_msg_nofication();
 		}
+	};
+
+	ODPWindow.prototype._show_msg_nofication = function(){
+		var notify = $('#odp-notify-bottom');
+		var c = notify.data('count');
+		if(!c) c = 0;
+		c++;
+		notify.data('count',c);
+		notify.text( c + 'new message '+ ((c)?'s':'') + ' available');
+		notify.fadeIn(500);
+
+	};
+
+	ODPWindow.prototype._hide_msg_nofication = function(){
+		notify = $('#odp-notify-bottom');
+		notify.fadeOut(500);
+		notify.data('count',0);
 	};
 
 	ODPWindow.prototype.update = function(w) {
